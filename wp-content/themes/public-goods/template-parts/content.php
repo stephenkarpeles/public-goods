835deb3abaa13a2e517bb7b90a846d6ddd430cdf
Stephen Karpeles <skarpeles@bluechipww.com>
2018-10-14 23:20:56 -0500

    Blog landing layout

--- wp-content/themes/public-goods/template-parts/content.php
+++ wp-content/themes/public-goods/template-parts/content.php
@@ -10,33 +10,36 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
 
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="entry-meta">
-				<?php
-				public_goods_posted_on();
-				public_goods_posted_by();
-				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
-
-	<?php public_goods_post_thumbnail(); ?>
+	<div class="entry-image">
+	  <?php public_goods_post_thumbnail(); ?>
+	</div>
 
 	<div class="entry-content">
+		<header class="entry-header">
+			<?php
+			if ( is_singular() ) :
+				the_title( '<h1 class="entry-title">', '</h1>' );
+			else :
+				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			endif;
+
+			if ( 'post' === get_post_type() ) :
+				?>
+				<div class="entry-meta">
+					<?php
+					public_goods_posted_on();
+					public_goods_posted_by();
+					?>
+				</div><!-- .entry-meta -->
+			<?php endif; ?>
+		</header><!-- .entry-header -->
+
 		<?php
 		the_content( sprintf(
 			wp_kses(
 				/* translators: %s: Name of current post. Only visible to screen readers */
-				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'public-goods' ),
+				__( 'Read More<span class="screen-reader-text"> "%s"</span>', 'public-goods' ),
 				array(
 					'span' => array(
 						'class' => array(),
@@ -51,9 +54,10 @@
 			'after'  => '</div>',
 		) );
 		?>
+
+		<footer class="entry-footer">
+			<?php public_goods_entry_footer(); ?>
+		</footer><!-- .entry-footer -->
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<?php public_goods_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
